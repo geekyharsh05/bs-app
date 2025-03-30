d1bc22dec2f1a2b89fdcaaa4ff007531542ef0a8
Harsh <hp424805@gmail.com>
2025-03-30 07:21:58 +0530

    Enhance RootLayout by integrating React Query for data fetching, implementing authentication checks, and updating navigation structure to include tabs and authentication screens.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,45 @@
 import SafeScreen from "@/components/safe-screen";
-import { Stack } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import Toast from "react-native-toast-message";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React, { useEffect } from "react";
+import { checkAuth } from "@/utils/auth-helpers";
+import { useAuthStore } from "@/store/auth-store";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
+  const router = useRouter();
+  const segments = useSegments();
+  const { user, token } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, []); 
+
+  useEffect(() => {
+    const isAuthPage = segments[0] === "(auth)";
+    const isLoggedIn = !!user && !!token;
+
+    if (!isAuthPage && !isLoggedIn) router.replace("/(auth)");
+    else if (isAuthPage && isLoggedIn) router.replace("/(tabs)");
+
+  }, [user, token, segments]);
+
   return (
-    <SafeAreaProvider>
-      <SafeScreen>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="index" />
-          <Stack.Screen name="(auth)" />
-        </Stack>
-      </SafeScreen>
-      <Toast />
-      <StatusBar style="dark" />
-    </SafeAreaProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <SafeScreen>
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="(tabs)" />
+            <Stack.Screen name="(auth)" />
+          </Stack>
+        </SafeScreen>
+        <Toast />
+        <StatusBar style="dark" />
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
