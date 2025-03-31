1ae24f5875079767bdfb64ac001c159b072dfb39
Harsh <hp424805@gmail.com>
2025-03-31 08:34:51 +0530

    Integrate SplashScreen handling and custom font loading in RootLayout; adjust header title font size in home styles for improved UI consistency.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
 import SafeScreen from "@/components/safe-screen";
-import { Stack, useRouter, useSegments } from "expo-router";
+import { SplashScreen, Stack, useRouter, useSegments } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import Toast from "react-native-toast-message";
@@ -7,17 +7,28 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React, { useEffect } from "react";
 import { checkAuth } from "@/utils/auth-helpers";
 import { useAuthStore } from "@/store/auth-store";
+import { useFonts } from "expo-font";
 
 const queryClient = new QueryClient();
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const router = useRouter();
   const segments = useSegments();
   const { user, token } = useAuthStore();
 
+  const [fontsLoaded] = useFonts({
+    "JetBrainsMono-Medium": require("../assets/fonts/JetBrainsMono-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded]);
+
   useEffect(() => {
     checkAuth();
-  }, []); 
+  }, []);
 
   useEffect(() => {
     const isAuthPage = segments[0] === "(auth)";
@@ -25,7 +36,6 @@ export default function RootLayout() {
 
     if (!isAuthPage && !isLoggedIn) router.replace("/(auth)");
     else if (isAuthPage && isLoggedIn) router.replace("/(tabs)");
-
   }, [user, token, segments]);
 
   return (
--- assets/styles/home.styles.ts
+++ assets/styles/home.styles.ts
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   headerTitle: {
-    fontSize: 24,
+    fontSize: 23,
     fontFamily: "JetBrainsMono-Medium",
     letterSpacing: 0.5,
     color: COLORS.primary,
