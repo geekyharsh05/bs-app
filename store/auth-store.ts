35b5cce541c820a7e14135fdd459dd14bedb1424
Harsh <hp424805@gmail.com>
2025-03-31 08:24:39 +0530

    Add utility functions for date formatting and enhance auth store with isCheckingAuth state

--- store/auth-store.ts
+++ store/auth-store.ts
@@ -3,6 +3,7 @@ import { create } from 'zustand';
 interface AuthState {
   user: any | null;
   token: string | null;
+  isCheckingAuth: boolean,
   setUser: (user: any) => void;
   setToken: (token: string | null) => void;
   clearAuth: () => void;
@@ -11,6 +12,7 @@ interface AuthState {
 export const useAuthStore = create<AuthState>((set) => ({
   user: null,
   token: null,
+  isCheckingAuth: true,
   setUser: (user) => set({ user }),
   setToken: (token) => set({ token }),
   clearAuth: () => set({ token: null, user: null }),
--- utils/auth-helpers.ts
+++ utils/auth-helpers.ts
@@ -2,10 +2,16 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useAuthStore } from '@/store/auth-store';
 
 export const checkAuth = async () => {
-  const token = await AsyncStorage.getItem('token');
-  const userJson = await AsyncStorage.getItem('user');
-  if (token && userJson) {
-    useAuthStore.setState({ token, user: JSON.parse(userJson) });
+  try {
+    const token = await AsyncStorage.getItem('token');
+    const userJson = await AsyncStorage.getItem('user');
+    if (token && userJson) {
+      useAuthStore.setState({ token, user: JSON.parse(userJson) });
+    }
+  } catch (error) {
+    console.log("Auth failed", error)
+  } finally {
+    useAuthStore.setState({isCheckingAuth: false})
   }
 };
 
